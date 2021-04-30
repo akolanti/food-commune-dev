6c3dc8b2367a10707f6671a1d509437b415e0ac2
Aaron <mac@Aaronss-MacBook-Pro.local>
2021-04-29 22:16:18 -0400

    changes for netlify

--- src/Components/RestaurantList/Restuarents.js
+++ src/Components/RestaurantList/Restuarents.js
@@ -15,7 +15,7 @@ const Restuarents = (props) => {
         <p className="card-text text-secondary">
         {props.Resto.res_name} Delivery Fee . 15-20 min
         </p>
-        <a href={`restaurant/${props.Resto._id}`} className="btn btn-outline-success">Order now</a>
+        <a href={`/restaurant/${props.Resto._id}`} className="btn btn-outline-success">Order now</a>
     </div>
 </div>
 </div>
